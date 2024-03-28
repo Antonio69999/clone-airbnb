bd2b65d63fee6e04133d5f227866c810b7e42a58
Antonio69999 <antoine.chabeauti@gmail.com>
2024-03-28 16:30:20 +0100

    fully added home

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,3 +1,63 @@
-export default function AddressRoute() {
-  return <h1>Address Route</h1>;
+"use client";
+
+import { useCountries } from "@/app/lib/getCountries";
+import {
+  Select,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+} from "@/components/ui/select";
+import dynamic from "next/dynamic";
+import { CreationBottomBar } from "@/app/components/CreationBottomBar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useState } from "react";
+import { createLocation } from "@/app/actions";
+
+export default function AddressRoute({ params }: { params: { id: string } }) {
+  const { getAllCountries } = useCountries();
+  const [locationValue, setLocationValue] = useState("");
+
+  const LazyMap = dynamic(() => import("@/app/components/Map"), {
+    ssr: false,
+    loading: () => <Skeleton className="h-[50vh] w-full" />,
+  });
+
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
+          Where is your Home located
+        </h2>
+      </div>
+
+      <form action={createLocation}>
+        <input type="hidden" name="homeId" value={params.id} />
+        <input type="hidden" name="countryValue" value={locationValue} />
+        <div className="w-3/5 mx-auto mb-36">
+          <div className="mb-5">
+            <Select required onValueChange={(value) => setLocationValue(value)}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a Country" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Countries</SelectLabel>
+                  {getAllCountries().map((item) => (
+                    <SelectItem key={item.value} value={item.value}>
+                      {item.flag} {item.label} ({item.region})
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <LazyMap locationValue={locationValue}></LazyMap>
+        </div>
+        <CreationBottomBar></CreationBottomBar>
+      </form>
+    </>
+  );
 }
