cadca1bd39a862ab7f2ce49891de6b34fba71dc1
Antonio69999 <antoine.chabeauti@gmail.com>
2024-03-29 14:27:41 +0100

    display homes

---
+++ app/components/ListingCard.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { useCountries } from "../lib/getCountries";
+import Link from "next/link";
+
+interface iAppProps {
+  imagePath: string;
+  description: string;
+  location: string;
+  price: number;
+}
+
+export function ListingCard({
+  description,
+  imagePath,
+  location,
+  price,
+}: iAppProps) {
+  const { getCountryByValue } = useCountries();
+  const country = getCountryByValue(location);
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-72">
+        <Image
+          src={`https://nuvsmjhvdfmmuoxemoep.supabase.co/storage/v1/object/public/images/${imagePath}`}
+          alt="Image of House"
+          fill
+          className="rounded-lg h-full object-cover mb-3"
+        ></Image>
+      </div>
+      <Link href={"/"}>
+        <h3 className=" font-medium text-base">
+          {country?.flag} {country?.label} ({country?.region})
+        </h3>
+        <p className="text-muted-foreground text-sm line-clamp-2">
+          {description}
+        </p>
+        <p className="pt-2 text-muted-foreground">
+          <span className=" font-medium text-black">€{price}</span> / Night
+        </p>
+      </Link>
+    </div>
+  );
+}
