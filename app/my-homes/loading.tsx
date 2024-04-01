0e389fe4cc6c38e00ac9fa31d6702242d18f953d
Antonio69999 <antoine.chabeauti@gmail.com>
2024-04-01 12:09:01 +0200

    homes listing

--- app/my-homes/loading.tsx
+++ app/my-homes/loading.tsx
@@ -1,9 +1,9 @@
 import { SkeltonCard } from "../components/SkeletonCard";
 
-export default function FavouriteLoading() {
+export default function MyHomesLoading() {
   return (
     <section className="container mx-auto px-5 lg:px-10 mt-10">
-      <h2 className="text-3xl font-semibold tracking-tight">Your Favourites</h2>
+      <h2 className="text-3xl font-semibold tracking-tight">Your Homes</h2>
 
       <div className="grid lg:grid-cols-4 sm:grid-cols-3 md:grid-cols-3  grid-cols-1 gap-8 mt-8">
         <SkeltonCard></SkeltonCard>
